93080e01162bdb3e879cee7baec1987154db53f8
Süleyman Kayabaşı <82765835+suleymankayabasi@users.noreply.github.com>
2022-07-31 16:31:13 +0300

    Initial commit

---
+++ src/Singleton.java
@@ -0,0 +1,17 @@
+class Singleton {
+
+    private static Singleton singletonInstance = null;
+
+    public String string;
+
+    private Singleton(){
+        string = "Hello I am Singleton";
+    }
+
+    public static Singleton getInstance(){
+        if (singletonInstance == null){
+            singletonInstance = new Singleton();
+        }
+        return singletonInstance;
+    }
+}
